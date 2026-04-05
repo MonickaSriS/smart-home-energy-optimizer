9e7beaa8d5e38fc3df6df5bafb7a7b3dbd5936f2
MonickaSriS <monickasri85@gmail.com>
2026-04-05 21:50:28 +0530

    Fixed HF structure

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from environment import SmartHomeEnergyEnv
 from openenv_core import create_app
-
+from environment import SmartHomeEnergyEnv, EnergyAction, EnergyObservation
 
 
 def main():
-    app = create_app(SmartHomeEnergyEnv)
+    app = create_app(
+        SmartHomeEnergyEnv,
+        EnergyAction,
+        EnergyObservation
+    )
     app.run(host="0.0.0.0", port=8000)
 
 
