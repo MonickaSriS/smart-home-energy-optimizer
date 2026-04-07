5653ea63b79447e9912fe6e38eab3fe76ec81bc7
MonickaSriS <monickasri85@gmail.com>
2026-04-07 21:26:44 +0530

    Fixed code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from openenv.core import create_app  # updated import
 from environment import SmartHomeEnergyEnv
 from models import Action as EnergyAction, Observation as EnergyObservation
+from dotenv import load_dotenv
+load_dotenv()
 
 app = create_app(
     SmartHomeEnergyEnv,
