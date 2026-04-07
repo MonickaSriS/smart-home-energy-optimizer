8daedb86a37caf80e8564456adfc431edd6c3a4d
MonickaSriS <monickasri85@gmail.com>
2026-04-07 21:05:03 +0530

    Fixed code

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
-from openenv_core import create_app
+from openenv.core import create_app  # updated import
 from environment import SmartHomeEnergyEnv
 from models import Action as EnergyAction, Observation as EnergyObservation
-import uvicorn
 
-
-def main():
-    app = create_app(
-        SmartHomeEnergyEnv,
-        EnergyAction,
-        EnergyObservation
-    )
-
-    uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-if __name__ == "__main__":
-    main()
+app = create_app(
+    SmartHomeEnergyEnv,
+    EnergyAction,
+    EnergyObservation
+)
